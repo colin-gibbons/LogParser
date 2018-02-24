a399c43c49f2627b83afc6b1c6790410f56a4ce7
Colin Gibbons <33758619+coling@users.noreply.github.com>
2018-02-24 02:04:28 -0600

    started main method, initial site connection

--- ParseLogs.py
+++ ParseLogs.py
@@ -1,5 +1,18 @@
+import urllib2
+import os
+
 def main():
-    print("main")
+    url = "https://s3.amazonaws.com/tcmg476/http_access_log"
+
+    fileName = url.split('/')[-1]
+    logFile = open(fileName, 'wb')
+    u = urllib2.urlopen(url)
+    metaData = u.info()
+    fileSizeKB = int(metaData.getheaders("Content-Length")[0]) / 1000
+
+    print("Downloading \"%s\" (%s KB)" % (fileName, fileSizeKB))
+
+
 
 if __name__ == "__main__":
     main()
