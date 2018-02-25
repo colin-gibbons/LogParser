1c0199cf70b6581786d67fcfc72e5a43d68d3e5d
Colin Gibbons <33758619+coling@users.noreply.github.com>
2018-02-24 23:07:08 -0600

    Converted to Python 3 Now can download log file (with progress bar)

--- ParseLogs.py
+++ ParseLogs.py
@@ -1,18 +1,36 @@
-import urllib2
+from urllib.request import urlopen
 import os
 
 def main():
     url = "https://s3.amazonaws.com/tcmg476/http_access_log"
-
     fileName = url.split('/')[-1]
     logFile = open(fileName, 'wb')
-    u = urllib2.urlopen(url)
-    metaData = u.info()
-    fileSizeKB = int(metaData.getheaders("Content-Length")[0]) / 1000
 
-    print("Downloading \"%s\" (%s KB)" % (fileName, fileSizeKB))
+    # Download Log File (with progress bar)
+    # https://stackoverflow.com/questions/22676/how-do-i-download-a-file-over-http-using-python
+    with urlopen(url) as stream:
+        metaData = dict(stream.getheaders())
+        fileSize = int(metaData["Content-Length"])
 
 
+        print(fileSize)
+        print("Downloading \"%s\" (%s KB)" % (fileName, fileSize / 1000))
+
+        currentFileSize = 0
+        blockSize = 8192
+        while currentFileSize/1000 < 10000:
+            buffer = stream.read(blockSize)
+            if not buffer:
+                break
+
+            currentFileSize += len(buffer)
+            logFile.write(buffer)
+            status = r"%10d [%3.2f%%]" % (currentFileSize, currentFileSize*100. / fileSize)
+            status = status + chr(8)*(len(status) + 1)
+
+            print(status, end="")
+
+    logFile.close()
 
 if __name__ == "__main__":
     main()
