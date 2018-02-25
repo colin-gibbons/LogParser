559fb5f826a3048d645b2eb5c0dc683521bae737
Colin Gibbons <33758619+coling@users.noreply.github.com>
2018-02-25 01:25:52 -0600

    Moved .log download to getDataFile() Added check for cached .log in Main() Renamed local data file

--- ParseLogs.py
+++ ParseLogs.py
@@ -1,36 +1,46 @@
 from urllib.request import urlopen
 import os
 
+url = "https://s3.amazonaws.com/tcmg476/http_access_log"
+fileName = "http.log"
+
+def getDataFile(): 
+    with open(fileName, 'wb') as logFile:
+        with urlopen(url) as stream:
+            fileSize = stream.length
+
+            # https://stackoverflow.com/questions/22676/how-do-i-download-a-file-over-http-using-python
+            print("Downloading \"%s\" (%s KB)..." % (fileName, fileSize / 1000))
+
+            currentFileSize = 0
+            blockSize = 8192
+            while currentFileSize < fileSize:
+                buffer = stream.read(blockSize)
+                if not buffer:
+                    break
+
+                currentFileSize += len(buffer)
+                logFile.write(buffer)
+                status = r"%10d [%3.2f%%]" % (currentFileSize, currentFileSize*100. / fileSize)
+                status = status + chr(8)*(len(status) + 1)
+
+                print(status, end="")
+            
+            print("", end="\n") # reset print trailing char
+
+
 def main():
-    url = "https://s3.amazonaws.com/tcmg476/http_access_log"
-    fileName = url.split('/')[-1]
-    logFile = open(fileName, 'wb')
-
-    # Download Log File (with progress bar)
-    # https://stackoverflow.com/questions/22676/how-do-i-download-a-file-over-http-using-python
-    with urlopen(url) as stream:
-        metaData = dict(stream.getheaders())
-        fileSize = int(metaData["Content-Length"])
-
-
-        print(fileSize)
-        print("Downloading \"%s\" (%s KB)" % (fileName, fileSize / 1000))
-
-        currentFileSize = 0
-        blockSize = 8192
-        while currentFileSize/1000 < 10000:
-            buffer = stream.read(blockSize)
-            if not buffer:
-                break
-
-            currentFileSize += len(buffer)
-            logFile.write(buffer)
-            status = r"%10d [%3.2f%%]" % (currentFileSize, currentFileSize*100. / fileSize)
-            status = status + chr(8)*(len(status) + 1)
-
-            print(status, end="")
-
-    logFile.close()
+    # check if file exists before re-downloading
+    if not os.path.exists(fileName):
+        print("No cached " + fileName + " found.\nDownloading from: " + url)
+        getDataFile()
+    else:
+        print("Using cached " + fileName " file.")
+    
+    with open(fileName, 'rb') as logFile:
+        print("parse")
+        
+     
 
 if __name__ == "__main__":
     main()
